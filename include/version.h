d556928ac7e8fde21b6ba492d8878c7f09666880
Marcin Spoczynski <marcin@spoczynski.com>
2024-11-08 18:17:53 -0800

    Bump to version 0.99

--- include/version.h
+++ include/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERS "TuneFinder 0.3"
-#define VSTRING "TuneFinder  0.3 (11.01.2024)\r\n"
-#define VERSTAG "\0$VER: TuneFinder  0.3 (11.01.2024)"
+#define VERS "TuneFinder 0.99"
+#define VSTRING "TuneFinder  0.99 (08.11.2024)\r\n"
+#define VERSTAG "\0$VER: TuneFinder  0.99 (08.11.2024)"
 
 #endif /* VERSION_H */
