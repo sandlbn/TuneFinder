57ff80a385fe09a693731257bcf0b988b2168372
Marcin Spoczynski <marcin@spoczynski.com>
2024-11-24 09:32:38 -0800

    Small fixed for icon readme and example country
    - Updated german readme
    - Updated docs icon
    - Updated example countries (removed non ASCII characters)
    - Updated Install script for contributors

--- include/version.h
+++ include/version.h
@@ -5,5 +5,5 @@
 #define VSTRING "TuneFinder  1.2 (11.23.2024)\r\n"
 #define VERSTAG "\0$VER: TuneFinder  1.2 (11.23.2024)"
 #define AUTHOR "Marcin Spoczynski"
-#define TRANSLATION "-German: Thomas Blatt\n-Italian: Samir Hawamdeh\n ICONS: Thomas Blatt\n"
+#define TRANSLATION "-German: Thomas Blatt\n-Italian: Samir Hawamdeh\n Icons: Thomas Blatt\n"
 #endif
