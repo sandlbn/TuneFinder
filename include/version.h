4e6fb87d33d60a1141c6b311010e47d90be108e7
sandlbn <marcin@spoczynski.com>
2024-12-09 22:12:48 -0800

    Update French translation by Eric Tarzin LUCZYSZYN

--- include/version.h
+++ include/version.h
@@ -5,7 +5,7 @@
 #define VSTRING "TuneFinder  1.3a (12.09.2024)\r\n"
 #define VERSTAG "\0$VER: TuneFinder  1.3a (12.09.2024)"
 #define AUTHOR "Marcin Spoczynski"
-#define TRANSLATION "-German: Thomas Blatt\n-Italian: Samir Hawamdeh\n-French: Eric 'Tarinz' LUCZYSZYN \nIcons: Thomas Blatt\n"
+#define TRANSLATION "-German: Thomas Blatt\n-Italian: Samir Hawamdeh\n-French: Eric 'Tarzin' LUCZYSZYN \nIcons: Thomas Blatt\n"
 __attribute__((section(".text"))) UBYTE VString[] =
        "" VERSTAG " \r\n";
 #endif
